79e9de6ffe141234cf432a935352ca4cd4328d96
Ismail Tawil <itawil165@gmail.com>
2022-05-22 21:33:41 -0500

    Initial commit

---
+++ src/test/java/scripts/_01_Apple_Test.java
@@ -0,0 +1,53 @@
+package scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class _01_Apple_Test {
+
+    // Declare your driver
+    WebDriver driver;
+
+    // 1. Initial setup
+    @BeforeMethod
+    public void setup() {
+        driver = Driver.getDriver();
+    }
+
+    // 2. Validation of test
+    @Test
+    public void testAppleTitle() {
+        driver.get("https://www.apple.com/");
+
+        String expectedTitle = "Apple"; // requirement
+        String actualTitle = driver.getTitle(); // development
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void testAppleURL() {
+        driver.get("https://www.apple.com/");
+
+        String expectedURL = "https://www.apple.com/"; // requirement
+        String actualURL = driver.getCurrentUrl(); // development
+
+        Assert.assertEquals(actualURL, expectedURL);
+    }
+
+    @Test
+    public void X() {
+        // Used for known issues
+        Assert.fail("Failed on purpose");
+    }
+
+    // 3. Teardown actions
+    @AfterMethod
+    public void teardown() {
+        Driver.quitDriver();
+    }
+}
