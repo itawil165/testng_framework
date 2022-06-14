8efb4caac80fecd91ef87113b6e714bccc97eff4
Ismail Tawil <itawil165@gmail.com>
2022-06-14 18:56:25 -0500

    Initial commit

--- src/test/java/scripts/_07_DropdownTest.java
+++ src/test/java/scripts/_07_DropdownTest.java
@@ -7,7 +7,7 @@ import utilities.Waiter;
 
 public class _07_DropdownTest extends Base {
 
-     /*
+    /*
     Go to https://www.facebook.com/
     Click on “Create New Account” link
     Enter below information and click on Sign Up button
@@ -19,7 +19,7 @@ public class _07_DropdownTest extends Base {
     Gender = Male
     Validate below error message is displayed with expected text
     Please choose a more secure password. It should be longer than 6 characters, unique to you, and difficult for others to guess.
-     */
+    */
 
     @Test(priority = 1, description = "TC763: Validate FaceBook sign in error message for password")
     public void testUnsecurePasswordErrorMessage(){
@@ -33,30 +33,30 @@ public class _07_DropdownTest extends Base {
         facebookPage.emailReEnterInputBox.sendKeys("johndoe55555@gmail.com");
         facebookPage.passwordInputBox.sendKeys("abcd1234");
 
-        //To be able to select an option from dropdown we need an instance of Select class
-        //Select monthSelector = new Select(facebookPage.monthDropdown);
-        //monthSelector.selectByValue("4"); // selection by value
-        //monthSelector.selectByVisibleText("Apr"); // selection by visible text
-        //monthSelector.selectByIndex(3); // selection by index - index starts from zero
+//        // To be able to select an option from dropdown we need an instance of Select class
+//        Select monthSelector = new Select(facebookPage.monthDropdown);
+//        monthSelector.selectByValue("4"); // selection by value
+//        monthSelector.selectByVisibleText("Apr"); // selection by visible text
+//        monthSelector.selectByIndex(3); // selection by index - index starts from zero
 
         DropdownHandler.selectOptionByIndex(facebookPage.monthDropdown, 3);
 
-        //Select day as 1
-        //Select daySelector = new Select(facebookPage.dayDropdown);
-        //daySelector.selectByVisibleText("1");
-        DropdownHandler.selectOptionByVisibleText(facebookPage.dayDropdown, "1");
-
-        //Select year as 1990
-        //Select yearSelector = new Select(facebookPage.yearDropdown);
-        //yearSelector.selectByValue("1990");
-        DropdownHandler.selectOptionByValue(facebookPage.yearDropdown, "1990");
+//        // Select day as 1
+//        Select daySelector = new Select(facebookPage.dayDropdown);
+//        daySelector.selectByVisibleText("1");
+//        DropdownHandler.selectOptionByVisibleText(facebookPage.dayDropdown, "1");
+//
+//        // Select year as 1990
+//        Select yearSelector = new Select(facebookPage.yearDropdown);
+//        yearSelector.selectByValue("1990");
+//        DropdownHandler.selectOptionByValue(facebookPage.yearDropdown, "1990");
 
         facebookPage.genderRadioButtons.get(1).click();
         facebookPage.signUpButton.click();
 
         String expectedMessage = "Please choose a more secure password. It should be longer than 6 characters, unique to you, and difficult for others to guess.";
 
-        //new WebDriverWait(driver, 30).until(ExpectedConditions.textToBePresentInElement(facebookPage.errorMessage, expectedMessage));
+//        new WebDriverWait(driver, 30).until(ExpectedConditions.textToBePresentInElement(facebookPage.errorMessage, expectedMessage));
         Waiter.waitUntilTextToBePresentInElement(driver, 60, facebookPage.errorMessage, expectedMessage);
         Assert.assertEquals(facebookPage.errorMessage.getText(), expectedMessage, "Error message have issues!!!");
     }
@@ -67,7 +67,7 @@ public class _07_DropdownTest extends Base {
     Select “Flights” from “More travel” dropdown
     Select “Premium economy” from “Economy” dropdown
     Validate “Premium economy” is selected
-     */
+    */
 
     @Test(priority = 2, description = "TC578: Validate Expedia dropdowns")
     public void testExpediaDropdowns(){
@@ -90,7 +90,7 @@ public class _07_DropdownTest extends Base {
     Validate “Welcome to eBay Art” heading1
     Validate “Find Prints, Paintings, Posters, and More.” heading2
     NOTE: Validation includes headings to be displayed and texts to be exactly matching above texts
-     */
+    */
 
     @Test(priority = 3, description = "TC242: Validate Ebay dropdown")
     public void testEbayDropdown(){
