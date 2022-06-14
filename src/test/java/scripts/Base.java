f427943552a33b3946d757a927f83ced2d441446
Ismail Tawil <itawil165@gmail.com>
2022-06-14 18:54:12 -0500

    Initial commit

--- src/test/java/scripts/Base.java
+++ src/test/java/scripts/Base.java
@@ -1,6 +1,8 @@
 package scripts;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,20 +16,22 @@ import java.util.concurrent.TimeUnit;
 
 public class Base {
 
-        WebDriver driver;
-        WebDriverWait explicitWait;
-        Wait fluentWait;
-        SoftAssert softAssert;
-        EtsySearchPage etsySearchPage;
-        TGApplicationPage tgApplicationPage;
-        GoogleSearchPage googleSearchPage;
-        GoogleSearchResultPage googleSearchResultPage;
-        HeroAppPage heroAppPage;
-        FacebookPage facebookPage;
-        ExpediaPage expediaPage;
-        RediffHomePage rediffHomePage;
-        AmazonHomePage amazonHomePage;
-        TGHomePage tgHomePage;
+    WebDriver driver;
+    WebDriverWait explicitWait;
+    Wait fluentWait;
+    SoftAssert softAssert;
+    Actions actions;
+    JavascriptExecutor js;
+    EtsySearchPage etsySearchPage;
+    TGApplicationPage tgApplicationPage;
+    GoogleSearchPage googleSearchPage;
+    GoogleSearchResultPage googleSearchResultPage;
+    HeroAppPage heroAppPage;
+    FacebookPage facebookPage;
+    ExpediaPage expediaPage;
+    RediffHomePage rediffHomePage;
+    AmazonHomePage amazonHomePage;
+    TGHomePage tgHomePage;
 
         @BeforeMethod
         public void setup(){
@@ -35,6 +39,8 @@ public class Base {
             explicitWait = new WebDriverWait(driver, 30);
             fluentWait = new FluentWait(driver).withTimeout(30, TimeUnit.SECONDS).pollingEvery(2, TimeUnit.SECONDS).ignoring(Exception.class);
             softAssert = new SoftAssert();
+            actions = new Actions(driver);
+            js = (JavascriptExecutor) driver;
             etsySearchPage = new EtsySearchPage(driver);
             tgApplicationPage = new TGApplicationPage(driver);
             googleSearchPage = new GoogleSearchPage(driver);
