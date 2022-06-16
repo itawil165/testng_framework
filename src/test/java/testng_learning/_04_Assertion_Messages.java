077aad90de616123834feb07874cb62da4f27487
Ismail Tawil <itawil165@gmail.com>
2022-06-15 19:55:55 -0500

    Initial commit

---
+++ src/test/java/testng_learning/_04_Assertion_Messages.java
@@ -0,0 +1,19 @@
+package testng_learning;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _04_Assertion_Messages {
+/*
+    @Test
+    public void method1(){
+        // TestNG
+        Assert.assertTrue("condition", "message");
+        Assert.assertEquals("actual", "expected", "message");
+
+        // JUnit
+        org.junit.Assert.assertTrue("message", "condition");
+        org.junit.Assert.assertEquals("message", "expected", "actual");
+    }
+*/
+}
